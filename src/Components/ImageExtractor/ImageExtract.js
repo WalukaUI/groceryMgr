47aa795c322bb1904ce1a1f85d34e8af73d4f432
WalukaUI <cwaluka@yahoo.com>
2022-08-02 21:50:58 -0500

    image extract folder added

---
+++ src/Components/ImageExtractor/ImageExtract.js
@@ -0,0 +1,10 @@
+import { useState, useRef } from "react";
+import Tesseract from "tesseract.js";
+
+export default function ImageExe() {
+  return (
+    <>
+      <h1>images</h1>
+    </>
+  );
+}
---
+++ src/Components/Main.js
@@ -0,0 +1,9 @@
+import ImageExe from "./ImageExtractor/ImageExtract";
+
+export default function Main() {
+  return (
+    <>
+      <ImageExe />
+    </>
+  );
+}

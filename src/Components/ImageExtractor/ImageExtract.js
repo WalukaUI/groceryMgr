83b3464b0773f0ce64e4273b23ba03ee122be32f
WalukaUI <cwaluka@yahoo.com>
2022-08-05 23:54:56 -0500

    Readme

--- src/Components/ImageExtractor/ImageExtract.js
+++ src/Components/ImageExtractor/ImageExtract.js
@@ -2,9 +2,25 @@ import { useState, useRef } from "react";
 import Tesseract from "tesseract.js";
 
 export default function ImageExe() {
+  const [imagePath, setImagePath] = useState("");
+  const [text, setText] = useState("");
+
+  const handleChange = (event) => {
+    setImage(URL.createObjectURL(event.target.files[0]));
+  };
+
   return (
-    <>
-      <h1>images</h1>
-    </>
+    <div className="App">
+      <main className="App-main">
+        <h3>Actual image uploaded</h3>
+        <img src={imagePath} className="App-logo" alt="logo" />
+
+        <h3>Extracted text</h3>
+        <div className="text-box">
+          <p> {text} </p>
+        </div>
+        <input type="file" onChange={handleChange} />
+      </main>
+    </div>
   );
 }
